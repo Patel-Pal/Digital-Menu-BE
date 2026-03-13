a2f7b488d84a42410a3f9739ea82bff8c0e090ef
Pal Patel <patelpal.93130@gmail.com>
2026-03-13 14:00:52 +0530

    Update CORS origins to include new domain

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ io.on('connection', (socket) => {
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:5173', 'https://digital-menu-builder.vercel.app','https://digital-menu-fe.vercel.app','https://digital-menu-fe.vercel.app'],
+  origin: ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:5173', 'https://digital-menu-builder.vercel.app','https://digital-menu-fe.vercel.app','https://digitalmenu.devinpro.co.in'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
